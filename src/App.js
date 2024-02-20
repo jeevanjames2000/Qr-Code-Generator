c0a9d3cdeb7dcdd1351e7e394bb82cd607308597
jeevansekhar1 <123068913+jeevansekhar1@users.noreply.github.com>
2024-02-20 15:15:02 +0530

    qr code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Grid, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+import QRCode from "react-qr-code";
 
-function App() {
+export default function App() {
+  const [qrCode, setQrCode] = useState("");
+  const [input, setInput] = useState("");
+
+  function handleGenerateQrCode() {
+    setQrCode(input);
+    setInput("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Grid container>
+      <Grid
+        item
+        container
+        xs={12}
+        display={"flex"}
+        justifyContent={"center"}
+        spacing={4}
+      >
+        <Grid item container justifyContent={"center"} xs={12}>
+          <h1>QR CODE GENERATOR</h1>
+        </Grid>
+        <Grid
+          item
+          container
+          display={"flex"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          xs={12}
+          spacing={2}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Grid item alignItems={"center"}>
+            <TextField
+              onChange={(e) => setInput(e.target.value)}
+              type="text"
+              name="qr-code"
+              value={input}
+              placeholder="Enter your value here"
+            />
+          </Grid>
+          <Grid item>
+            <Button
+              disabled={input && input.trim() !== "" ? false : true}
+              variant="outlined"
+              onClick={handleGenerateQrCode}
+            >
+              Generate
+            </Button>
+          </Grid>
+        </Grid>
+        <Grid
+          item
+          container
+          xs={12}
+          spacing={2}
+          justifyContent={"center"}
+          display={"flex"}
+        >
+          <Grid item>
+            <QRCode
+              id="qr-code-value"
+              value={qrCode}
+              size={400}
+              bgColor="#fff"
+            />
+            <Typography variant="h4" align="center">
+              Scan to View
+            </Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
-
-export default App;
